146a5d30a34a2a9f72c0b74b1e8af64517d5d115
Citlali Hernandez Lopez <89164697+citlalih1421@users.noreply.github.com>
2025-09-01 19:27:22 -0500

    prework submission

---
+++ README.md
@@ -0,0 +1,27 @@
+## iOS102 Pre-work: Introduce Yourself
+
+### App Description
+
+App that takes a user's information (full name, the name of the school they attend and what year they are in. It also takes in how many pets they have and if they want more. 
+
+### App Walk-though 
+
+<div style="position: relative; padding-bottom: 213.01775147928996%; height: 0;"><iframe src="https://www.loom.com/embed/6157a55be5ad4d72a78cfb2a8b3a50ab?sid=56ff041a-7fb4-48a5-aa8b-d15575a11a9b" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>
+<!-- <img src="YOUR_GIF_URL_HERE" width=200><br> OR <img src="YOUR_GIF_PATH" width=200><br> -->
+
+
+### Required Features
+
+- [x] 1. App displays an image of a school's logo
+- [x] 2. App has three textfields for first, last, and school names
+- [x] 3. App has a segmented control that changes student year
+- [x] 4. Number of pet matches label is increased/decreased by stepper
+- [x] 5. Switch makes a statement about wanting more pets or not(true/false) 
+- [x] 6. Introduce yourself button shows alert box with an introduciton and dismiss button
+
+### Optional Features
+
+- [ ] 1. User can tap a button to change the color of the background view
+- [ ] 3. User can select on additional buttons that provide more info about the user. Example: more textfields, a different alert box, etc.
+- [ ] 4. Any stylistic changes that are not default options
+
--- iOS102CodepathPrework/ViewController.swift
+++ iOS102CodepathPrework/ViewController.swift
@@ -9,9 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+   
+    
+    @IBOutlet weak var petNumberLabel: UILabel!
+    @IBOutlet weak var petNumberStepper: UIStepper!
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        petNumberLabel.text = "\(Int(sender.value))"
+    }
+    
+    
+    @IBAction func introduceDidTap(_ sender: UIButton) {
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(petNumberLabel.text!) pets. It is \(morePetsSwitch.isOn) that I want more pets."
+        
+        print(introduction)
+        
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
     }
 
 
